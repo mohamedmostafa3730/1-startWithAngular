dc385bef0bb4d0e9882b2a268f6d4a94c6f30202
Mohamed Mostafa <mohamedmostafa3730@gmail.com>
2025-01-11 15:34:50 +0200

    update 1:2 install and initialize     => bootstrap & fontawesome

--- startWithAngular/src/app/app.component.ts
+++ startWithAngular/src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./navbar/navbar.component";
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
+import { PortfolioComponent } from "./portfolio/portfolio.component";
+import { ContactComponent } from "./contact/contact.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, HomeComponent, AboutComponent, PortfolioComponent, ContactComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
